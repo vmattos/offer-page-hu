7019c54013ebe335e6178de831d90a8a42dd25d8
Vitor Mattos <vitor.mattos@vtex.com.br>
2016-06-30 18:43:50 -0300

    Displays a better server log

--- server/server.js
+++ server/server.js
@@ -126,7 +126,7 @@ app.use((req, res, next) => {
 // start app
 app.listen(serverConfig.port, (error) => {
   if (!error) {
-    console.log(`MERN is running on port: ${serverConfig.port}! Build something amazing!`); // eslint-disable-line
+    console.log(`Server is running on port: ${serverConfig.port}`); // eslint-disable-line
   }
 });
 
