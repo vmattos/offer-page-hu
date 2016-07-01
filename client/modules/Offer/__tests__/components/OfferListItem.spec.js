a2f64d7745583da8124421ec2529824fd135a020
Vitor Mattos <vitor.mattos@vtex.com.br>
2016-07-01 10:29:03 -0300

    Fixing tests

--- client/modules/Offer/__tests__/components/OfferListItem.spec.js
+++ client/modules/Offer/__tests__/components/OfferListItem.spec.js
@@ -3,7 +3,16 @@ import test from 'ava';
 import OfferListItem from '../../components/OfferListItem/OfferListItem';
 import { mountWithIntl } from '../../../../util/react-intl-test-helper';
 
-const offer = { id: 0, title: 'salty vegas nights' };
+const offer = {
+  id: 0,
+  title: 'salty vegas nights',
+  photos: ['images/photo1.jpg'],
+  options: [
+    {
+      price: 2300,
+    },
+  ],
+};
 const props = {
   offer,
 };
