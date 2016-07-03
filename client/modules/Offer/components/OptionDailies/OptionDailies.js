bfa3ac6fc167ff1320c128fb469d6151c13c3ebd
Vitor Mattos <vitor.mattos@vtex.com.br>
2016-07-03 20:04:25 -0300

    Styles dailies section

--- client/modules/Offer/components/OptionDailies/OptionDailies.js
+++ client/modules/Offer/components/OptionDailies/OptionDailies.js
@@ -6,9 +6,17 @@ class OptionDailies extends Component {
   render() {
     const option = this.props.option;
 
+    const glyphClasses = 'glyphicon glyphicon-calendar';
+
     return (
       <div className={style.Section}>
-        <p>{option.daily} diária{option.daily == 1 ? '' : 's'}</p>
+        <p>
+          <span className={style.Dailies}>{option.daily}</span>
+          {' '}
+          <span className={style.Description}>
+            Diária{option.daily == 1 ? '' : 's'}
+          </span>
+        </p>
       </div>
     );
   }
