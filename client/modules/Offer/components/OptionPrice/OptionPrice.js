9e2ddfb613981c5af8bf62d32f01a3351cbf9e76
Vitor Mattos <vitor.mattos@vtex.com.br>
2016-07-03 21:16:55 -0300

    Styles OptionPrice

--- client/modules/Offer/components/OptionPrice/OptionPrice.js
+++ client/modules/Offer/components/OptionPrice/OptionPrice.js
@@ -3,13 +3,34 @@ import React, { PropTypes, Component } from 'react';
 import style from './OptionPrice.css';
 
 class OptionPrice extends Component {
-  render() {
-    const option = this.props.option;
+  constructor(props) {
+    super(props);
+  }
 
+  componentWillMount() {
+    this.formattedPrice = this.formatPrice(this.props.option.price)
+  }
+
+  formatPrice(price) {
+    return price
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  }
+
+  render() {
     return (
       <div className={style.Section}>
-        <p>Por apenas:</p>
-        <p>R$ <span>{option.price}</span></p>
+        <p>
+          Por apenas:
+          {' '}
+          <span>
+            R$ <span className={style.Price}>{this.formattedPrice}</span>
+          </span>
+          {' '}
+          + taxas em até 10x
+        </p>
+
+        <button className={style.Confirm}>Quero ir</button>
       </div>
     );
   }
