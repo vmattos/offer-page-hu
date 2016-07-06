71134e8b465c86f9c36fc76b28970b40f1f5f19b
Vitor Mattos <vitor.mattos@vtex.com.br>
2016-07-06 09:02:06 -0300

    Tests getVisibility selector

--- client/modules/Offer/__tests__/VisibilityReducer.spec.js
+++ client/modules/Offer/__tests__/VisibilityReducer.spec.js
@@ -1,6 +1,6 @@
 import test from 'ava';
 import { reducerTest } from 'redux-ava';
-import visibilityReducer, { VisibilityReducer } from '../VisibilityReducer';
+import visibilityReducer, { getVisibility } from '../VisibilityReducer';
 import { setOriginFilter, setDailyFilter } from '../VisibilityActions';
 
 test('action for SET_ORIGIN_FILTER is working', reducerTest(
@@ -29,3 +29,17 @@ test('action for SET_DAILY_FILTER is working', reducerTest(
   },
 ));
 
+test('getVisibility selector', t => {
+  t.deepEqual(
+    getVisibility({
+      visibility: {
+        origin: 'Rio de Janeiro',
+        daily: '5',
+      }
+    }),
+    {
+      origin: 'Rio de Janeiro',
+      daily: '5',
+    }
+  );
+});
