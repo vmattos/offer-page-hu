24ca0c9e38d7471402444ed3fc87174478689b21
Vitor Mattos <vitor.mattos@vtex.com.br>
2016-07-06 05:45:37 -0300

    Loads routes synchronously

--- client/routes.js
+++ client/routes.js
@@ -2,41 +2,17 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './modules/App/App';
 
-// require.ensure polyfill for node
-if (typeof require.ensure !== 'function') {
-  require.ensure = function requireModule(deps, callback) {
-    callback(require);
-  };
-}
+import OfferListPage from './modules/Offer/pages/OfferListPage/OfferListPage';
+import OfferDetailsPage from './modules/Offer/pages/OfferDetailsPage/OfferDetailsPage';
 
-/* Workaround for async react routes to work with react-hot-reloader till
-  https://github.com/reactjs/react-router/issues/2182 and
-  https://github.com/gaearon/react-hot-loader/issues/288 is fixed.
- */
-if (process.env.NODE_ENV !== 'production') {
-  // Require async routes only in development for react-hot-reloader to work.
-  require('./modules/Offer/pages/OfferListPage/OfferListPage');
-  require('./modules/Offer/pages/OfferDetailsPage/OfferDetailsPage');
-}
-
-// react-router setup with code-splitting
-// More info: http://blog.mxstbr.com/2016/01/react-apps-with-pages/
 export default (
   <Route path="/" component={App}>
     <IndexRoute
-      getComponent={(nextState, cb) => {
-        require.ensure([], require => {
-          cb(null, require('./modules/Offer/pages/OfferListPage/OfferListPage').default);
-        });
-      }}
+      component={OfferListPage}
     />
     <Route
       path="/offers/:id"
-      getComponent={(nextState, cb) => {
-        require.ensure([], require => {
-          cb(null, require('./modules/Offer/pages/OfferDetailsPage/OfferDetailsPage').default);
-        });
-      }}
+      component={OfferDetailsPage}
     />
   </Route>
 );
