9f2a0baff826cebba529596b29b805bd7b359b0c
Vitor Mattos <vitor.mattos@vtex.com.br>
2016-07-06 00:07:23 -0300

    Inverts control of ordering options logic

--- client/modules/Offer/components/OptionsBody/OptionsBody.js
+++ client/modules/Offer/components/OptionsBody/OptionsBody.js
@@ -3,25 +3,12 @@ import React, { PropTypes, Component } from 'react';
 import Option from '../Option/Option';
 
 class OptionsBody extends Component{
-  componentWillMount() {
-    this.visibleOptions = this.orderOptions(this.props.visibleOptions);
-  }
-
-  orderOptions(options) {
-    const optionsClone = [...options]
-    optionsClone.sort(function sortByPrice(a, b) {
-      if (a.price > b.price) return 1;
-      if (a.price < b.price) return -1;
-      return 0;
-    });
-    return optionsClone;
-  }
-
   render() {
+    const { visibleOptions } = this.props;
     return (
       <div>
         {
-          this.visibleOptions.map((option) => (
+          visibleOptions.map((option) => (
             <Option key={option.id} option={option} />
           ))
         }
