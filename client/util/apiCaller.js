6c251351cdbf71d5d8e5b517948afeffbbcff6a9
Vitor Mattos <vitor.mattos@vtex.com.br>
2016-07-06 04:40:08 -0300

    Refactors apiCaller

--- client/util/apiCaller.js
+++ client/util/apiCaller.js
@@ -1,9 +1,14 @@
 import fetch from 'isomorphic-fetch';
 import Config from '../../server/config';
 
-export const API_URL = (typeof window === 'undefined' || process.env.NODE_ENV === 'test') ?
-  process.env.BASE_URL || (`http://localhost:${process.env.PORT || Config.port}/api`) :
-  '/api';
+export const API_URL = (function() {
+  const isServer = typeof window === 'undefined';
+  const isTest = process.env.NODE_ENV === 'test';
+  if (isServer || isTest) {
+    return process.env.BASE_URL || (`http://localhost:${process.env.PORT || Config.port}/api`);
+  }
+  return '/api'
+})();
 
 export default function callApi(endpoint, method = 'get', body) {
   return fetch(`${API_URL}/${endpoint}`, {
